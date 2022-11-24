0bd34c71a32baca958b71db28f11350680348034
holentommy <37346345+holentommy@users.noreply.github.com>
2022-11-24 06:31:27 +0100

    Update Microsoft.PowerShell_profile.ps1
    Adding PSReadLineOption config to Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -3,3 +3,7 @@ Invoke-Expression (&starship init powershell)
 
 # Initialize oh-my-posh.
 Invoke-Expression (&oh-my-posh init powershell)
+
+# Configure PSReadLineOption.
+Set-PSReadLineOption -PredictionSource History
+Set-PSReadLineOption -PredictionViewStyle listview
